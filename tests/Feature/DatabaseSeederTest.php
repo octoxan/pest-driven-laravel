06a257ee76e875de61d637422dd83aabf35b1ba2
Xan <octoxan@users.noreply.github.com>
2024-03-16 17:58:01 -0400

    Episode 27 - Creating and Testing Database Seeders in Laravel

--- tests/Feature/DatabaseSeederTest.php
+++ tests/Feature/DatabaseSeederTest.php
@@ -1,16 +1,19 @@
 <?php
 
 use App\Models\Course;
+use App\Models\User;
+use App\Models\Video;
+use Illuminate\Support\Facades\App;
 
 it('adds given courses', function () {
     // Assert
-    $this->assertDatabaseCount('courses', 0);
+    $this->assertDatabaseCount(Course::class, 0);
 
     // Act
     $this->artisan('db:seed');
 
     // Assert
-    $this->assertDatabaseCount('courses', 3);
+    $this->assertDatabaseCount(Course::class, 3);
     $this->assertDatabaseHas('courses', ['title' => 'Laravel For Beginners']);
     $this->assertDatabaseHas('courses', ['title' => 'Advanced Laravel']);
     $this->assertDatabaseHas('courses', ['title' => 'TDD The Laravel Way']);
@@ -26,15 +29,64 @@ it('adds given courses only once', function () {
 });
 
 it('adds given videos', function () {
-    // Arrange
+    // Assert
+    $this->assertDatabaseCount(Video::class, 0);
 
-    // Act && Assert
+    // Act
+    $this->artisan('db:seed');
 
+    // Assert
+    $laravelForBeginnersCourse = Course::where('title', 'Laravel For Beginners')->first();
+    $advancedLaravelCourse = Course::where('title', 'Advanced Laravel')->first();
+    $tddTheLaravelWayCourse = Course::where('title', 'TDD The Laravel Way')->firstOrFail();
+
+    // Assert
+    expect($laravelForBeginnersCourse)
+        ->videos
+        ->toHaveCount(3);
+
+    expect($advancedLaravelCourse)
+        ->videos
+        ->toHaveCount(3);
+
+    expect($tddTheLaravelWayCourse)
+        ->videos
+        ->toHaveCount(2);
 });
 
 it('adds given videos only once', function () {
+    // Assert
+    $this->assertDatabaseCount(Video::class, 0);
+
+    // Act
+    $this->artisan('db:seed');
+    $this->artisan('db:seed');
+
+    // Assert
+    $this->assertDatabaseCount(Video::class, 8);
+});
+
+it('adds local test user', function () {
     // Arrange
+    App::partialMock()->shouldReceive('environment')->andReturn('local');
 
-    // Act && Assert
+    // Assert
+    $this->assertDatabaseCount(User::class, 0);
 
+    // Act
+    $this->artisan('db:seed');
+
+    // Assert
+    $this->assertDatabaseCount(User::class, 1);
+});
+
+it('does not add test user for production', function () {
+    // Assert
+    $this->assertDatabaseCount(User::class, 0);
+
+    // Act
+    $this->artisan('db:seed');
+
+    // Assert
+    $this->assertDatabaseCount(User::class, 0);
 });
