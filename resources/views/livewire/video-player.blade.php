daefa400f9d818ee9243866657e4903e193b983b
Xan <octoxan@users.noreply.github.com>
2024-03-15 22:20:38 -0400

    Episode 24 - Testing Livewire Components Better

--- resources/views/livewire/video-player.blade.php
+++ resources/views/livewire/video-player.blade.php
@@ -1,6 +1,11 @@
 <div>
     <h3>{{ $video->title }} ({{ $video->getReadableDuration() }})</h3>
     <p>{{ $video->description }}</p>
+    @if (auth()->user()->watchedVideos()->where('video_id', $video->id)->exists())
+        <button wire:click="markVideoAsNotCompleted">Mark as not completed</button>
+    @else
+        <button wire:click="markVideoAsCompleted">Mark as completed</button>
+    @endif
     <iframe src="https://player.vimeo.com/video/{{ $video->vimeo_id }}"
             webkitallowfullscreen
             mozallowfullscreen
--- tests/Feature/Livewire/VideoPlayerTest.php
+++ tests/Feature/Livewire/VideoPlayerTest.php
@@ -78,7 +78,10 @@ it('marks video as completed', function () {
     loginAsUser($user);
 
     Livewire::test(VideoPlayer::class, ['video' => $course->videos()->first()])
-        ->call('markVideoAsCompleted');
+        ->assertMethodWired('markVideoAsCompleted')
+        ->call('markVideoAsCompleted')
+        ->assertMethodNotWired('markVideoAsCompleted')
+        ->assertMethodWired('markVideoAsNotCompleted');
 
     // Assert
     $user->refresh();
@@ -104,7 +107,10 @@ it('marks video as not completed', function () {
     loginAsUser($user);
 
     Livewire::test(VideoPlayer::class, ['video' => $course->videos()->first()])
-        ->call('markVideoAsNotCompleted');
+        ->assertMethodWired('markVideoAsNotCompleted')
+        ->call('markVideoAsNotCompleted')
+        ->assertMethodNotWired('markVideoAsNotCompleted')
+        ->assertMethodWired('markVideoAsCompleted');
 
     // Assert
     $user->refresh();
