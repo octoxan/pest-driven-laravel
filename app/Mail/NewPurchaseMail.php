937a649890760254bc432202b3a6c1a516a26d8a
Xan <octoxan@users.noreply.github.com>
2024-03-17 20:26:41 -0400

    Episode 37 - Refactoring Code and Testing Email Content

--- app/Mail/NewPurchaseMail.php
+++ app/Mail/NewPurchaseMail.php
@@ -2,52 +2,17 @@
 
 namespace App\Mail;
 
-use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldQueue;
+use App\Models\Course;
 use Illuminate\Mail\Mailable;
-use Illuminate\Mail\Mailables\Content;
-use Illuminate\Mail\Mailables\Envelope;
-use Illuminate\Queue\SerializesModels;
 
 class NewPurchaseMail extends Mailable
 {
-    use Queueable, SerializesModels;
-
-    /**
-     * Create a new message instance.
-     */
-    public function __construct()
+    public function __construct(public Course $course)
     {
-        //
     }
 
-    /**
-     * Get the message envelope.
-     */
-    public function envelope(): Envelope
+    public function build(): self
     {
-        return new Envelope(
-            subject: 'New Purchase Mail',
-        );
-    }
-
-    /**
-     * Get the message content definition.
-     */
-    public function content(): Content
-    {
-        return new Content(
-            view: 'view.name',
-        );
-    }
-
-    /**
-     * Get the attachments for the message.
-     *
-     * @return array<int, \Illuminate\Mail\Mailables\Attachment>
-     */
-    public function attachments(): array
-    {
-        return [];
+        return $this->markdown('emails.new-purchase');
     }
 }
---
+++ resources/views/emails/new-purchase.blade.php
@@ -0,0 +1,14 @@
+@component('mail::message')
+    # Thanks for purchasing {{ $course->title }}
+
+    If this is your first purchase on {{ config('app.name') }}, then a new account was created for you, and you just need to
+    reset your password.
+    Have fun with your new course.
+
+    @component('mail::button', ['url' => route('login')])
+        Login
+    @endcomponent
+
+    Thanks,<br>
+    {{ config('app.name') }}
+@endcomponent
